b0185969025d92feba38e5c1d3ede80b03e18733
Hai Nguyen <quochai.kstn@gmail.com>
2024-08-02 00:15:03 +0700

    redis scan function

--- go-redis/hash_test.go
+++ go-redis/hash_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"log"
 	"testing"
 
 	"github.com/go-redis/redis/v8"
@@ -61,4 +63,43 @@ func Test_hash_then_delete(t *testing.T) {
 
 		})
 	}
+}
+
+func Test_scan_play(t *testing.T) {
+	client := redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+	// remove all keys
+
+	userMap := map[string]string{
+		"name":  "John",
+		"email": "email value",
+		"City":  "city value", // Will be ingored by the scan because tag is not matching
+		"extra": "extra value",
+	}
+	ctx := context.Background()
+	// delete user:1 key
+	_, err := client.Del(ctx, "user:1").Result()
+	if err != nil {
+		log.Fatal("del failed", err)
+	}
+
+	// set the hash to redis
+	_, err = client.HSet(ctx, "user:1", userMap).Result()
+	if err != nil {
+		log.Fatal("hset failed", err)
+	}
+
+	// get the hash from redis
+	kvs, err := client.HGetAll(ctx, "user:1").Result()
+	if err != nil {
+		log.Fatal("hgetall failed", err)
+	}
+	// log kvs
+	log.Println("kvs", kvs)
+	// scan the hash to a user struct
+	var user User
+	err = client.HGetAll(ctx, "user:1").Scan(&user)
+	if err != nil {
+		log.Fatal("scan error")
+	}
+	log.Println("user info", user)
 }
